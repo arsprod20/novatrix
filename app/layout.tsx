3feeeed6c8cc4f358ddab4a21638abe61ee4e404
Amadou Sow <sow.amadou-ibrahima@ugb.edu.sn>
2025-06-05 20:02:14 -0400

    feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,17 +3,11 @@ import { GeistSans, GeistMono } from "geist/font";
 import "./globals.css";
 import Header from '@/components/Header';
 import Footer from "@/components/Footer";
-import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Portfolio - Amadou Sow",
   description: "Fullstack Web Developer",
-  themeColor: [
-    { media: '(prefers-color-scheme: light)', color: '#00eaff' },
-    { media: '(prefers-color-scheme: dark)', color: '#010409' },
-  ],
 };
-
 
 export default function RootLayout({
   children,
@@ -22,15 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <Head>
-        <meta name="theme-color" media="(prefers-color-scheme: light)" content="#00eaff" />
-        <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#010409" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-      </Head>
       <body className={`${GeistSans.variable} ${GeistMono.variable} antialiased flex flex-col min-h-screen`}>
         <Header />
-        <main className="flex-grow pt-20">{children}</main>
+        <main className="flex-grow pt-20"> {/* pt-20 pour l'espace sous le header fixe */}
+          {children}
+        </main>
         <Footer />
       </body>
     </html>
